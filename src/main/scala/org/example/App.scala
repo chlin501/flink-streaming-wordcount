12a322191e1bc7d59622da7ea95e2e1a48dd7c8a
test <clin4j@googlemail.com>
2016-05-29 16:16:33 +0800

    count total records.

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -1,9 +1,17 @@
 package org.example
 
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.api.scala._ 
+import org.apache.flink.api.common.accumulators.IntCounter
+import org.apache.flink.api.common.functions.RichMapFunction
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+import org.slf4j.LoggerFactory
 
 object App {
+
+  val RECORD_COUNTER = "record_counter"
+  
   def main(args: Array[String]) {
     App.create.io("/tmp/in")("/tmp/out")
   }
@@ -12,15 +20,44 @@ object App {
 }
 class App {
 
+  import App._
+
+  val log = LoggerFactory.getLogger(classOf[App])
+
   def io(in: String)(out: String) {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     val stream = env.readTextFile(in)
-    stream.flatMap { _.toLowerCase.split("\\W+") filter { _.nonEmpty } }.
-           map { (_, 1) }.
-           keyBy(0).
-           sum(1).
-           writeAsText(out)
-    env.execute("parallel-wordcount")
+    stream.map(new CounterMapFn)
+    val result = env.execute("parallel-count")
+    val intResult = result.getAccumulatorResult(RECORD_COUNTER).
+                           asInstanceOf[Int]
+    log.info("total records counted -> "+intResult)
+    // write intResult to zk so consumer side can check within its own 
+    // accumulator, which is global within the job being executed.
+    // when a consumer (thread) finds after its execution reaching the 
+    // total count found at zk, then write to zk so procuder can start 
+    // the next job/ step.
   }
 }
 
+class CounterMapFn extends RichMapFunction[String, String] {
+
+  import App._
+
+  var counter = new IntCounter
+  var serialVersionUID = 1L
+
+  @throws(classOf[Exception])
+  override def open(conf: Configuration) {
+    getRuntimeContext.addAccumulator(RECORD_COUNTER, counter)  
+  }
+
+  @throws(classOf[Exception])
+  override def map(value: String): String = {
+    counter.add(1)
+    value
+  }
+
+  @throws(classOf[Exception])
+  override def close() { }
+}
